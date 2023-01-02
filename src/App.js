ff28566d48a4d6682a871c5212f96059aa4307ea
harri37 <110083492+harri37@users.noreply.github.com>
2023-01-02 22:29:51 +1000

    progress

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
 import Home from "./pages/Home";
+import Header from "./components/Header";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import SweetTalk from "./pages/SweetTalk";
+import Shop from "./pages/Shop";
+import Events from "./pages/Events";
+import Cart from "./pages/Cart";
+import SweetTalkArticle from "./pages/SweetTalkArticle";
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <main>
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
-      </main>
-    </BrowserRouter>
+    <>
+      <BrowserRouter>
+        <Header />
+        <main>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/sweetTalk" element={<SweetTalk />} />
+            <Route path="/sweetTalk/:title" element={<SweetTalkArticle />} />
+            <Route path="/shop" element={<Shop />} />
+            <Route path="/events" element={<Events />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="*" element={<h1>404</h1>} />
+          </Routes>
+        </main>
+      </BrowserRouter>
+    </>
   );
 };
 
